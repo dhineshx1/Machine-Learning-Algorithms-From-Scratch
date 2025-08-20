5d533eeb1db292fa62fc157e6462cb62c6ffbd7b
dhineshx1 <dhineshx1@gmail.com>
2025-08-20 23:50:35 +0530

    Logistic regressor Test Added

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from neighbors import KNNClassifier,KNNRegressor
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from linear_regression import LinearRegressor
+from logistic_regressor import LogisticRegressor
 
 def test_KNNClassifier():
     iris = datasets.load_iris()
@@ -64,8 +66,27 @@ def test_linear_regression():
     print(f"  MAE  : {mae:.2f}")
     print(f"  R sqr   : {r2:.2f}")
 
+
+def test_logistic_regressor():
+    bc = datasets.load_breast_cancer()
+
+    X_train,X_test,y_train,y_test = train_test_split(bc.data,bc.target,random_state=42,test_size=0.8)
+
+    model = LogisticRegressor(lr=0.01, n_iter=2000)
+
+    model.fit(X_train,y_train)
+
+    y_pred = model.predict(X_test)
+
+    print(confusion_matrix(y_pred,y_test))
+    acc =np.sum(y_pred==y_test) / len(y_pred)
+    
+    print(acc)
+
+test_KNNClassifier()
 test_KNNRegressor()
 test_linear_regression()
+test_logistic_regressor()
 
 
 
